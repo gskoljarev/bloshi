fccb0c0f37eaf90c260362857f773e49d8019b8c
Goran Školjarev <gskoljarev@gmail.com>
2016-10-06 16:06:31 +0200

    Tweak initial bloshi settings

--- bloshi/bloshi/settings.py
+++ bloshi/bloshi/settings.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from os.path import join, abspath, dirname
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+here = lambda *x: join(abspath(dirname(__file__)), *x)
+PROJECT_ROOT = here("..",)
+root = lambda *x: join(abspath(PROJECT_ROOT), *x)
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = root('static')
