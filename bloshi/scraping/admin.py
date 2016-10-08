7ffc1f452925b0611def14e8048430ee17c8478a
Goran Školjarev <gskoljarev@gmail.com>
2016-10-08 16:40:54 +0200

    Expose TemporaryItem model to admin

--- bloshi/scraping/admin.py
+++ bloshi/scraping/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.db import models
 from django.forms import Textarea
 
-from .models import Spider
+from .models import Spider, TemporaryItem
 
 
 class SpiderAdmin(admin.ModelAdmin):
@@ -28,3 +28,4 @@ class SpiderAdmin(admin.ModelAdmin):
     }
 
 admin.site.register(Spider, SpiderAdmin)
+admin.site.register(TemporaryItem)
